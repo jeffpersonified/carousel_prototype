5c7fac63d97831d89d80c33e91a2eaaec319f32d
Jeff Smith <jeff@thoughtbot.com>
2014-09-23 11:32:35 -0400

    add tutorial paging and add carousel

--- carousel/TutorialViewController.swift
+++ carousel/TutorialViewController.swift
@@ -10,26 +10,32 @@ import UIKit
 
 class TutorialViewController: UIViewController {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var tutorialScrollView: UIScrollView!
+    @IBOutlet weak var firstWelcomeImageView: UIImageView!
+    @IBOutlet var tutorialSwipeGestureRecognizer: UISwipeGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tutorialScrollView.contentSize.width = firstWelcomeImageView.frame.size.width * 4
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func scrollViewDidEndDecelerating(tutorialScrollView: UIScrollView!) {
+        // Get the current page based on the scroll offset
+        var page : Int = Int(round(tutorialScrollView.contentOffset.x / 320))
+        
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
+        println(pageControl.currentPage)
     }
-    */
 
 }
